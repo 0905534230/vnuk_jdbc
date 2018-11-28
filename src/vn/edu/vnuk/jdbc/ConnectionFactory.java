30134d87b004a5992c859e3d71dbfaf405ebd87b
0905534230 <quan.hoang170203@vnuk.edu.vn>
2018-11-28 16:47:19 +0700

    ConnectionFactory

--- src/vn/edu/vnuk/jdbc/ConnectionFactory.java
+++ src/vn/edu/vnuk/jdbc/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package vn.edu.vnuk.jdbc;
 
-public class ConnectionFactory {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public Connection getConnection(){
+		try {
+			return DriverManager.getConnection(
+					"jdbc:mysql://localhost/vnuk_personnel",
+					"root",
+					"Luyn21Va5Ben1"
+			);		
+		}
+		catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
